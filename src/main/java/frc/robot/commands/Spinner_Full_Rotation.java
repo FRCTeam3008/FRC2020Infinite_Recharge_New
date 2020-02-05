a7fb0df9cdfa20e314d3ef1053dc3fde7054a211
Wilson Tran <FRCTeam3008@gmail.com>
2020-02-04 19:38:48 -1000

    working on full rotation button press

---
+++ src/main/java/frc/robot/commands/Spinner_Full_Rotation.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class Spinner_Full_Rotation extends CommandBase {
+  /**
+   * Creates a new Spinner_Full_Rotation.
+   */
+  public Spinner_Full_Rotation() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.spinner);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.spinner.storeColor();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
